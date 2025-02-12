565c851aa454d7d53cdb7b6e9e442e744be4a0d0
chezard <subhanmasimov02@gmail.com>
2025-02-12 09:38:13 +0300

    fix

--- src/main/java/com/chezard/mvncentral/CalculateService.java
+++ src/main/java/com/chezard/mvncentral/CalculateService.java
@@ -2,14 +2,12 @@ package com.chezard.mvncentral;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Service;
 
-@Service
 public class CalculateService {
 
     private static final Logger log = LoggerFactory.getLogger(CalculateService.class);
 
-    public static void calculate() {
+    public void calculate() {
         log.info("CalculateService.calculate() called");
     }
 
--- src/test/java/com/chezard/mvncentral/TestApplication.java
+++ src/test/java/com/chezard/mvncentral/TestApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MvnCentralApplication {
+public class TestApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MvnCentralApplication.class, args);
+        SpringApplication.run(TestApplication.class, args);
     }
 
 }
